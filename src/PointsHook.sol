32fcaca5b37696ee6ae1d9163ce1401527050ee3
dharmikumbhani <dharmikumbhani201@gmail.com>
2025-02-08 18:15:00 +0530

    Added all tests and scripts

--- src/PointsHook.sol
+++ src/PointsHook.sol
@@ -9,9 +9,14 @@ import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {PointsToken} from "./PointsToken.sol";
 
 contract PointsHook is BaseHook {
-    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+    PointsToken public pointsToken;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {
+        pointsToken = new PointsToken();
+    }
 
     function getHookPermissions()
         public
@@ -41,21 +46,76 @@ contract PointsHook is BaseHook {
     function afterSwap(
         address,
         PoolKey calldata key,
-        IPoolManager.SwapParams calldata,
+        IPoolManager.SwapParams calldata swapParams,
         BalanceDelta delta,
-        bytes calldata
-    ) external override returns (bytes4, int128) {
+        bytes calldata hookData
+    ) external override onlyPoolManager returns (bytes4, int128) {
+        // We only award points in the ETH/TOKEN pools.
+        if (!key.currency0.isAddressZero()) {
+            return (BaseHook.afterSwap.selector, 0);
+        }
+
+        // We only award points if the user is buying the TOKEN
+        if (!swapParams.zeroForOne) {
+            return (BaseHook.afterSwap.selector, 0);
+        }
+
+        // Let's figure out who's the user
+        address user = parseHookData(hookData);
+
+        // How much ETH are they spending?
+        uint256 ethSpendAmount = swapParams.amountSpecified < 0
+            ? uint256(-swapParams.amountSpecified)
+            : uint256(int256(-delta.amount0()));
+       
+        // And award the points!
+        awardPoints(user, ethSpendAmount);
+
         return (BaseHook.afterSwap.selector, 0);
     }
 
-    function afterAddLiquidity(
+     function afterAddLiquidity(
         address sender,
         PoolKey calldata key,
         IPoolManager.ModifyLiquidityParams calldata params,
         BalanceDelta delta,
         BalanceDelta feesAccrued,
         bytes calldata hookData
-    ) external override returns (bytes4, BalanceDelta) {
+    ) external override onlyPoolManager returns (bytes4, BalanceDelta) {
+        // We only award points in the ETH/TOKEN pools.
+        if (!key.currency0.isAddressZero()) {
+            return (BaseHook.afterAddLiquidity.selector, delta);
+        }
+
+        // Let's figure out who's the user
+        address user = parseHookData(hookData);
+
+        // How much ETH are they spending?
+        uint256 ethSpendAmount = uint256(int256(-delta.amount0()));
+
+        // And award the points!
+        awardPoints(user, ethSpendAmount);
+
         return (BaseHook.afterAddLiquidity.selector, delta);
+    }
+
+    function getPointsForAmount(
+        uint256 amount
+    ) internal pure returns (uint256) {
+        return amount; // 1:1 with ETH
+    }
+
+    function awardPoints(address to, uint256 amount) internal {
+        pointsToken.mint(to, getPointsForAmount(amount));
+    }
+
+    function getHookData(address user) public pure returns (bytes memory) {
+        return abi.encode(user);
+    }
+
+    function parseHookData(
+        bytes calldata data
+    ) public pure returns (address user) {
+        return abi.decode(data, (address));
     }
 }
--- src/PointsToken.sol
+++ src/PointsToken.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PointsToken is ERC20, Ownable {
-    constructor() ERC20("Points Token", "POINTS", 18) Ownable(msg.sender) {}
+    constructor() ERC20("Points Token", "POINTS") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
